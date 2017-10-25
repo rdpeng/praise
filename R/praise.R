14fde2f5a3b84b5116863b402fd4f056f167d7a6
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-25 15:18:51 +1100

    Add some examples to the praise function

--- R/praise.R
+++ R/praise.R
@@ -3,6 +3,10 @@
 #' @description Praise me at critical times in my life
 #'
 #' @export
+#'
+#' @examples
+#' ## This is how you use it
+#' praise()
 praise <- function() {
         print("You're the best!")
 }
